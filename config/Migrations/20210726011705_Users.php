edea4403b16fb6a15edd88d48ae429b25ae5124e
nahidul77 <nahidul.mee@gmail.com>
2021-07-26 07:37:57 +0600

    Users migration added

---
+++ config/Migrations/20210726011705_Users.php
@@ -0,0 +1,44 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Users extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('email', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('password', 'string', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+
+    public function down(){
+        $this->table('users')->drop()->save();
+    }
+}
